7143ad6531952faf97ad422efebde470b2e02fd0
Febrian Nugraha <febrian.14nugraha@gmail.com>
2025-06-23 20:49:16 +0700

    05: auth social

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,9 +1,18 @@
 import { Card } from "@/components/ui/card";
-import { SignInViews } from '@/modules/auth/ui/views/sign-in-views';
-const SignInPage = () => {
-  return (
-    <SignInViews/>
-  );
+import { auth } from "@/lib/auth";
+import { SignInViews } from "@/modules/auth/ui/views/sign-in-views";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+const SignInPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!!session) {
+    redirect("/");
+  }
+
+  return <SignInViews />;
 };
 
 export default SignInPage;

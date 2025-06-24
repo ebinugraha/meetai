c18082f56053a6c083bd68061e79fd4956de7876
Febrian Nugraha <febrian.14nugraha@gmail.com>
2025-06-24 15:36:24 +0700

    07: dashboard navbar

---
+++ src/modules/dashboard/ui/components/dashboard-navbar.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useSidebar } from "@/components/ui/sidebar";
+import { PanelLeft, PanelLeftCloseIcon, SearchIcon } from "lucide-react";
+import { DashboardCommand } from "./dashboard-command";
+import { useEffect, useState } from "react";
+
+export const DashboardNavbar = () => {
+  const { state, toggleSidebar, isMobile } = useSidebar();
+
+  const [open, setOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+
+      document.addEventListener("keydown", down);
+
+      return () => {
+        document.removeEventListener("keydown", down);
+      };
+    };
+  }, []);
+
+  return (
+    <>
+      <DashboardCommand open={open} setOpen={setOpen} />
+      <nav className="flex px-4 gap-x-2 items-center py-3 border-b bg-background">
+        <Button className="size-9" variant={"outline"} onClick={toggleSidebar}>
+          {state === "collapsed" || isMobile ? (
+            <PanelLeft />
+          ) : (
+            <PanelLeftCloseIcon />
+          )}
+        </Button>
+        <Button
+          className="h-9 w-[240px] justify-start font-normal text-muted-foreground hover:text-muted-foreground"
+          variant={"outline"}
+          onClick={() => setOpen((open) => !open)}
+        >
+          <SearchIcon />
+          Search
+          <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px]">
+            <span className="text-xs">ctrl </span>+ k
+          </kbd>
+        </Button>
+      </nav>
+    </>
+  );
+};
--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -12,10 +12,25 @@ import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { GenerateAvatar } from "@/components/generate-avatar";
 import { ChevronDown, CreditCard, LogOut } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { Skeleton } from "@/components/ui/skeleton";
+
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { Button } from "@/components/ui/button";
 
 export const DashboardUserButton = () => {
   const router = useRouter();
   const { data, isPending } = authClient.useSession();
+  const isMobile = useIsMobile();
 
   const onLogout = () => {
     authClient.signOut({
@@ -28,7 +43,44 @@ export const DashboardUserButton = () => {
   };
 
   if (isPending || !data?.user) {
-    return null;
+    return <DashboardUserButtonSkeleton />;
+  }
+
+  if (isMobile) {
+    return (
+      <Drawer>
+        <DrawerTrigger className="rounded-lg border gap-x-3 border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden">
+          {data.user.image ? (
+            <Avatar>
+              <AvatarImage src={data.user.image} />
+            </Avatar>
+          ) : (
+            <GenerateAvatar seed={data.user.name} variant="botttsNeutral" />
+          )}
+          <div className="flex flex-col gap-0.5 overflow-hidden flex-1 min-w-0 text-left">
+            <span className="text-sm truncate w-full">{data.user.name}</span>
+            <span className="text-xs truncate w-full">{data.user.email}</span>
+          </div>
+          <ChevronDown className="size-5" />
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>{data.user.name}</DrawerTitle>
+            <DrawerTitle>{data.user.email}</DrawerTitle>
+          </DrawerHeader>
+          <DrawerFooter>
+            <Button variant={"outline"} onClick={() => {}}>
+              <CreditCard className="size-4 text-black" />
+              Billing
+            </Button>
+            <Button variant={"outline"} onClick={() => {}}>
+              <LogOut className="size-4 text-black" />
+              Logout
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    );
   }
 
   return (
@@ -70,5 +122,17 @@ export const DashboardUserButton = () => {
         <DropdownMenuSeparator />
       </DropdownMenuContent>
     </DropdownMenu>
+  );
+};
+
+export const DashboardUserButtonSkeleton = () => {
+  return (
+    <div className="rounded-lg border gap-x-3 border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden">
+      <Skeleton className="rounded-full h-8 w-8" />
+      <div className="flex flex-col gap-1 overflow-hidden flex-1 min-w-0">
+        <Skeleton className="w-16 h-3 " />
+        <Skeleton className="w-28 h-3" />
+      </div>
+    </div>
   );
 };

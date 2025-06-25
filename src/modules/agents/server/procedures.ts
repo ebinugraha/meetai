334f1d3f85755cbbcbc3c9a5e46cc7daa3697758
Febrian Nugraha <febrian.14nugraha@gmail.com>
2025-06-25 12:44:35 +0700

    11: agents-form

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,13 +1,37 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import {
+  baseProcedure,
+  createTRPCRouter,
+  protectedBaseProcedure,
+} from "@/trpc/init";
+import { agentsSchema } from "../schemas";
+import { z } from "zod";
+import { eq } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
-  getMany: baseProcedure.query(async () => {
+  getOne: protectedBaseProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input }) => {
+      const [data] = await db
+        .select()
+        .from(agents)
+        .where(eq(agents.id, input.id));
+      return data;
+    }),
+  // next change to protectedBaseProcedure
+  getMany: protectedBaseProcedure.query(async () => {
     const data = await db.select().from(agents);
-
-    // throw new TRPCError({ code: "BAD_GATEWAY" });
-
     return data;
   }),
+  create: protectedBaseProcedure
+    .input(agentsSchema)
+    .mutation(async ({ input, ctx }) => {
+      const [createdAgent] = await db
+        .insert(agents)
+        .values({ ...input, userId: ctx.auth.user.id })
+        .returning();
+
+      return createdAgent;
+    }),
 });

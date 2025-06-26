7e1f653436780a4191d3b475508b79928164a80f
Febrian Nugraha <febrian.14nugraha@gmail.com>
2025-06-26 16:05:36 +0700

    15: agents update delte

--- src/modules/agents/schemas.ts
+++ src/modules/agents/schemas.ts
@@ -1,6 +1,10 @@
 import { z } from "zod";
 
 export const agentsSchema = z.object({
-    name: z.string().min(1, {message: "Name is required"}),
-    instruction: z.string().min(1, {message: "Instruction is required"})
-})
+  name: z.string().min(1, { message: "Name is required" }),
+  instruction: z.string().min(1, { message: "Instruction is required" }),
+});
+
+export const updateAgentsSchema = agentsSchema.extend({
+  id: z.string().min(1, { message: "Id is required" }),
+});
--- src/modules/agents/ui/views/agents-id-view.tsx
+++ src/modules/agents/ui/views/agents-id-view.tsx
@@ -2,12 +2,21 @@
 
 import { ErrorState } from "@/components/error-state";
 import { useTRPC } from "@/trpc/client";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import {
+  useMutation,
+  useQueryClient,
+  useSuspenseQuery,
+} from "@tanstack/react-query";
 import { AgentIdViewHeader } from "../components/agent-id-view-header";
 import { GenerateAvatar } from "@/components/generate-avatar";
 import { Badge } from "@/components/ui/badge";
 import { VideoIcon } from "lucide-react";
 import { Skeleton } from "@/components/ui/skeleton";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { useConfirm } from "@/hooks/use-confirm";
+import { useState } from "react";
+import { UpdateAgentDialog } from "../components/agent-update-dialog";
 
 interface Props {
   agentId: string;
@@ -15,6 +24,8 @@ interface Props {
 
 export const AgentIdView = ({ agentId }: Props) => {
   const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const router = useRouter();
 
   const { data } = useSuspenseQuery(
     trpc.agents.getOne.queryOptions({
@@ -22,39 +33,76 @@ export const AgentIdView = ({ agentId }: Props) => {
     })
   );
 
+  const removeAgents = useMutation(
+    trpc.agents.remove.mutationOptions({
+      onSuccess: async () => {
+        await queryClient.invalidateQueries(
+          trpc.agents.getMany.queryOptions({})
+        );
+        router.push("/agents");
+      },
+      onError: (error) => {
+        toast.error(error.message);
+      },
+    })
+  );
+
+  const [RemoveConfirmation, confirmRemove] = useConfirm(
+    "Are you sure?",
+    `The following action will remove ${data.meetingCount} associated meetings`
+  );
+
+  const [updateAgentDialogOpen, setUpdateAgentDialogOpen] = useState(false);
+
+  const handleRemove = async () => {
+    const ok = await confirmRemove();
+
+    if (!ok) return;
+
+    await removeAgents.mutateAsync({ id: agentId });
+  };
+
   return (
-    <div className="flex-1 flex flex-col pb-4 py-5 px-4 md:px-8 gap-y-4">
-      <AgentIdViewHeader
-        agentId={agentId}
-        agentName={data.name}
-        onEdit={() => {}}
-        onRemove={() => {}}
+    <>
+      <RemoveConfirmation />
+      <UpdateAgentDialog
+        onOpenChange={setUpdateAgentDialogOpen}
+        open={updateAgentDialogOpen}
+        initialsValues={data}
       />
-      <div className="bg-white rounded-lg border">
-        <div className="px-4 py-5 gap-y-5 flex flex-col col-span-5">
-          <div className="flex items-center gap-x-6">
-            <GenerateAvatar
-              seed={data.name}
-              variant="botttsNeutral"
-              className="size-10"
-            />
-            <h2 className="font-medium">{data.name}</h2>
-          </div>
-          <Badge
-            variant={"outline"}
-            className="flex items-center gap-x-2 [&>svg]:size-4"
-          >
-            <VideoIcon className="text-blue-600" />
-            {data.meetingCount}{" "}
-            {data.meetingCount === 1 ? "meeting" : "meetings"}
-          </Badge>
-          <div className="flex flex-col gap-y-1">
-            <h1 className="font-medium text-lg">Instruction</h1>
-            <h1 className="text-neutral-800">{data.instruction}</h1>
+      <div className="flex-1 flex flex-col pb-4 py-5 px-4 md:px-8 gap-y-4">
+        <AgentIdViewHeader
+          agentId={agentId}
+          agentName={data.name}
+          onEdit={() => setUpdateAgentDialogOpen(true)}
+          onRemove={handleRemove}
+        />
+        <div className="bg-white rounded-lg border">
+          <div className="px-4 py-5 gap-y-5 flex flex-col col-span-5">
+            <div className="flex items-center gap-x-6">
+              <GenerateAvatar
+                seed={data.name}
+                variant="botttsNeutral"
+                className="size-10"
+              />
+              <h2 className="font-medium">{data.name}</h2>
+            </div>
+            <Badge
+              variant={"outline"}
+              className="flex items-center gap-x-2 [&>svg]:size-4"
+            >
+              <VideoIcon className="text-blue-600" />
+              {data.meetingCount}{" "}
+              {data.meetingCount === 1 ? "meeting" : "meetings"}
+            </Badge>
+            <div className="flex flex-col gap-y-1">
+              <h1 className="font-medium text-lg">Instruction</h1>
+              <h1 className="text-neutral-800">{data.instruction}</h1>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
@@ -69,7 +117,6 @@ export const AgentsIdErrorViews = () => {
 export const AgentsIdLoadingViews = () => {
   return (
     <div className="px-8">
-
       <div className="space-y-1">
         {/* Header Skeleton */}
         <div className="flex items-center justify-between">
